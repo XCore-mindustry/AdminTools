603186347b83e09a795189437d193160e7bfac90
nekonya <senoyabure@gmail.com>
2023-03-09 23:59:25 +0300

    JoinClicker

--- src/admintools/AdminTools.java
+++ src/admintools/AdminTools.java
@@ -1,6 +1,15 @@
 package admintools;
 
+import arc.Core;
+import arc.Events;
+import arc.scene.event.Touchable;
+import arc.scene.ui.layout.WidgetGroup;
+import arc.util.Time;
+import mindustry.Vars;
+import mindustry.game.EventType;
+import mindustry.gen.Icon;
 import mindustry.mod.*;
+import mindustry.ui.Styles;
 
 import static mindustry.Vars.netClient;
 
@@ -8,5 +17,24 @@ public class AdminTools extends Mod {
     @Override
     public void init() {
         netClient.addPacketHandler("give_ban_data", content -> new BanDialog(content).show());
+
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+            WidgetGroup group = new WidgetGroup();
+            group.setFillParent(true);
+            group.touchable = Touchable.childrenOnly;
+            group.visible(() -> true);
+            Core.scene.add(group);
+
+            TravelerFragment travelerFragment = new TravelerFragment();
+            travelerFragment.button(Icon.modePvp, Styles.defaulti, () -> Vars.ui.join.connect("130.61.52.25", 6567)).uniformX().uniformY().fill();
+            travelerFragment.button(Icon.modeSurvival, Styles.defaulti, () -> Vars.ui.join.connect("130.61.52.25", 6568)).uniformX().uniformY().fill();
+            travelerFragment.button(Icon.planet, Styles.defaulti, () -> Vars.ui.join.connect("130.61.52.25", 6569)).uniformX().uniformY().fill();
+            travelerFragment.button(Icon.modeAttack, Styles.defaulti, () -> Vars.ui.join.connect("130.61.52.25", 6570)).uniformX().uniformY().fill();
+            travelerFragment.button(Icon.star, Styles.defaulti, () -> Vars.ui.join.connect("130.61.52.25", 6572)).uniformX().uniformY().fill();
+
+            group.addChild(travelerFragment);
+
+            Time.run(10, () -> travelerFragment.setPosition(group.getWidth() / 2, group.getHeight() / 1.5f));
+        });
     }
 }
---
+++ src/admintools/TravelerFragment.java
@@ -0,0 +1,70 @@
+package admintools;
+
+import arc.input.KeyCode;
+import arc.math.Mathf;
+import arc.math.geom.Vec2;
+import arc.scene.event.InputEvent;
+import arc.scene.event.InputListener;
+import arc.scene.ui.ImageButton;
+import arc.scene.ui.layout.Table;
+import arc.util.Tmp;
+import mindustry.gen.Icon;
+import mindustry.ui.Styles;
+
+public class TravelerFragment extends Table {
+
+    public static float draggedAlpha = 0.45f;
+
+    public ImageButton dragger;
+    public boolean isDragging = false;
+
+    public TravelerFragment() {
+        dragger = new ImageButton(Icon.move, Styles.defaulti);
+        add(dragger).uniformX().uniformY().fill();
+
+        dragger.addListener(new InputListener() {
+
+            protected float dragx, dragy;
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, KeyCode button) {
+                dragx = x;
+                dragy = y;
+                isDragging = true;
+                return true;
+            }
+
+            @Override
+            public void touchDragged(InputEvent event, float x, float y, int pointer) {
+                positionParent(x, y);
+            }
+
+            @Override
+            public void touchUp(InputEvent e, float x, float y, int pointer, KeyCode button) {
+                isDragging = false;
+            }
+
+        });
+
+        update(() -> {
+            color.a = isDragging ? draggedAlpha : 1f;
+
+            Vec2 pos = localToParentCoordinates(Tmp.v1.set(0, 0));
+            setPosition(
+                    Mathf.clamp(pos.x, getPrefWidth() / 2, parent.getWidth() - getPrefWidth() / 2),
+                    Mathf.clamp(pos.y, getPrefHeight() / 2, parent.getHeight() - getPrefHeight() / 2)
+            );
+        });
+    }
+
+    public void positionParent(float x, float y) {
+        if (parent == null) return;
+
+        Vec2 pos = dragger.localToAscendantCoordinates(parent, Tmp.v1.set(x, y));
+        setPosition(
+                Mathf.clamp(pos.x, getPrefWidth() / 2, parent.getWidth() - getPrefWidth() / 2),
+                Mathf.clamp(pos.y, getPrefHeight() / 2, parent.getHeight() - getPrefHeight() / 2)
+        );
+    }
+
+}
