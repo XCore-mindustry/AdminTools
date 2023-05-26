52f0cdb70a21a6ef2f3693d50e5a898d6560e148
Terr <terr2048@gmail.com>
2023-05-26 10:10:27 +0300

    update history format

--- src/admintools/HistoryEntry.java
+++ src/admintools/HistoryEntry.java
@@ -1,50 +1,54 @@
 package admintools;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import arc.util.Reflect;
+import arc.util.Strings;
 import mindustry.Vars;
+import mindustry.gen.Iconc;
+import mindustry.world.Block;
 
 public class HistoryEntry {
     public short x;
     public short y;
-
     public String name;
     public String uuid;
+    public boolean valid;
     public short time;
     public short block;
+    public boolean destroy;
     public short rotation;
     public short config_type;
     public String config;
 
-    public boolean valid;
-
-    public HistoryEntry() {
+    public Block block() {
+        return Vars.content.block(block);
     }
 
-    public HistoryEntry(short x, short y, String name, String uuid, short time, short block, short rotation, short config_type, String config, boolean valid) {
-        this.x = x;
-        this.y = y;
-        this.name = name;
-        this.uuid = uuid;
-        this.time = time;
-        this.block = block;
-        this.rotation = rotation;
-        this.config_type = config_type;
-        this.config = config;
-    }
-
-    public String rotationAsString() {
-        if (!Vars.content.block(block).rotate)
-            return " ";
-        switch (rotation) {
-            case 0:
-                return "";
-            case 1:
-                return "";
-            case 2:
-                return "";
-            case 3:
-                return "";
-            default:
-                return "?";
+    public String blockEmoji() {
+        try {
+            if (block == 0) return destroy ? "[red]X[]" : "[green][]";
+            if (block >= 3 && block <= 11) return "[green]" + (block - 2) + "[]";
+            return (destroy ? "[red]" : "[]") + Reflect.get(Iconc.class, Strings.kebabToCamel(block().getContentType().name() + "-" + block().name)) + "[]";
+        } catch (Exception e) {
+            return "[scarlet]" + block + "[]";
         }
+    }
+
+    public Object rotationAsString() {
+        if (!block().rotate)
+            return null;
+        return switch (rotation) {
+            case 0 -> "";
+            case 1 -> "";
+            case 2 -> "";
+            case 3 -> "";
+            default -> "?";
+        };
+    }
+
+    public String timeAsString() {
+        return (valid ? "[white]" : "[gray]") + LocalTime.MIN.plusSeconds(time).format(DateTimeFormatter.ISO_LOCAL_TIME) + "[]";
     }
 }
--- src/admintools/HistoryFrame.java
+++ src/admintools/HistoryFrame.java
@@ -1,15 +1,8 @@
 package admintools;
 
 import arc.scene.ui.layout.Table;
-import arc.util.Reflect;
 import arc.util.Strings;
-import mindustry.Vars;
-import mindustry.content.Blocks;
-import mindustry.ctype.UnlockableContent;
 import mindustry.gen.Iconc;
-
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 public class HistoryFrame {
     public static Table table = new Table();
@@ -57,16 +50,16 @@ public class HistoryFrame {
 
         Table table1 = new Table();
         for (int i = iStart; i < iEnd; i++) {
-            var nya = st[i];
-            table1.add(Strings.stripColors(nya.name));
+            var state = st[i];
+            table1.add(Strings.stripColors(state.name));
             table1.add("  |  ");
-            table1.add((!nya.valid ? "[red]" : "") + emoji(Vars.content.block(nya.block)));
+            table1.add(state.blockEmoji());
             table1.add("  |  ");
-            table1.add((!nya.valid ? "[red]" : "") + nya.rotationAsString());
+            table1.add(state.rotationAsString() + "");
             table1.add("  |  ");
-            table1.add(nya.config.length() > 15 ? nya.config.substring(0, 15) : nya.config);
+            table1.add(state.config.length() > 15 ? state.config.substring(0, 15) : state.config);
             table1.add("  |  ");
-            table1.add((!nya.valid ? "[red]" : "") + LocalTime.MIN.plusSeconds(nya.time).format(DateTimeFormatter.ISO_LOCAL_TIME)); // я отойду // поесть
+            table1.add(state.timeAsString());
             table1.row();
         }
         table.add(table1);
@@ -79,17 +72,6 @@ public class HistoryFrame {
         int tdx = plus ? pageId + 1 : pageId - 1;
         if (openPage(tdx)) {
             pageId = tdx;
-        }
-    }
-
-    public static char emoji(UnlockableContent content) {
-        try {
-            return Reflect.get(Iconc.class, Strings.kebabToCamel(content.getContentType().name() + "-" + content.name));
-        } catch (Exception e) {
-            if (content == Blocks.air) {
-                return 'X';
-            }
-            return '?';
         }
     }
 }
